af632b94f6fc1f92dc3f5fde6af54e0a85825acb
TrevorRezac <trev.rezac@gmail.com>
2022-02-03 20:47:48 -0800

    current movie state displays to DOM

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
   const [directorForm, setDirectorForm] = useState('');
   const [yearForm, setYearForm] = useState('');
   const [genreForm, setGenreForm] = useState('');
-  const [colorForm, setColorForm] = useState('');
+  const [colorForm, setColorForm] = useState('lightcoral');
   const [allMovies, setAllMovies] = useState([]);
 
-  console.log('||', titleForm, directorForm, yearForm, genreForm, colorForm);
-  console.log('||', allMovies);
+  // console.log('||', titleForm, directorForm, yearForm, genreForm, colorForm);
+  // console.log('||', allMovies);
 
   function addMovie(newMovie) {
     const updatedMovies = [...allMovies, newMovie];
@@ -21,15 +21,24 @@ function App() {
 
   return (
     <div className="App">
-      <MovieForm 
-        titleForm={titleForm} 
-        setTitleForm={setTitleForm} 
-        directorForm={directorForm} 
-        setDirectorForm={setDirectorForm} 
-        yearForm={yearForm} setYearForm={setYearForm} 
-        genreForm={genreForm} setGenreForm={setGenreForm} 
-        colorForm={colorForm} setColorForm={setColorForm} 
-        addMovie={addMovie}/>
+      <div className='current-movie-container'>
+        <MovieForm 
+          titleForm={titleForm} 
+          setTitleForm={setTitleForm} 
+          directorForm={directorForm} 
+          setDirectorForm={setDirectorForm} 
+          yearForm={yearForm} setYearForm={setYearForm} 
+          genreForm={genreForm} setGenreForm={setGenreForm} 
+          colorForm={colorForm} setColorForm={setColorForm} 
+          addMovie={addMovie}/>
+        {}
+        <div className='movie-item' style={{ backgroundColor: colorForm }}>
+          <h3>{titleForm}</h3>
+          <p>Director: {directorForm}</p>
+          <p>Released: {yearForm}</p>
+          <p>Genre: {genreForm}</p>
+        </div>
+      </div>
     </div>
   );
 }
