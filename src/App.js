6abf750de24977c7f8fca878570896a7bc40adc7
TrevorRezac <trev.rezac@gmail.com>
2022-02-03 18:30:06 -0800

    minor form styling

--- src/App.js
+++ src/App.js
@@ -12,26 +12,28 @@ function App() {
 
   return (
     <div className="App">
-      <form>
-        Add a movie!
-        <label>
-          Title
-          <input required value={titleForm} onChange={e => setTitleForm(e.target.value)}></input>
-        </label>
-        <label>
-          Director
-          <input required value={directorForm} onChange={e => setDirectorForm(e.target.value)}></input>
-        </label>
-        <label>
-          Year
-          <input required value={yearForm} onChange={e => setYearForm(e.target.value)}></input>
-        </label>
-        <label>
-          Genre
-          <input required value={genreForm} onChange={e => setGenreForm(e.target.value)}></input>
-        </label>
-        <button>Add Movie</button>
-      </form>
+      <div className='form-container'>
+        <form>
+          Add a movie!
+          <label>
+            Title
+            <input required value={titleForm} onChange={e => setTitleForm(e.target.value)}></input>
+          </label>
+          <label>
+            Director
+            <input required value={directorForm} onChange={e => setDirectorForm(e.target.value)}></input>
+          </label>
+          <label>
+            Year
+            <input required value={yearForm} onChange={e => setYearForm(e.target.value)}></input>
+          </label>
+          <label>
+            Genre
+            <input required value={genreForm} onChange={e => setGenreForm(e.target.value)}></input>
+          </label>
+          <button>Add Movie</button>
+        </form>
+      </div>
     </div>
   );
 }
