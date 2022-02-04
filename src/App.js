ae9fe7b40017bb27f4d929b82127604cd058ed71
TrevorRezac <trev.rezac@gmail.com>
2022-02-03 19:25:52 -0800

    created MovieForm component, pushes new movie to state

--- src/App.js
+++ src/App.js
@@ -10,28 +10,8 @@ function App() {
   const [colorForm, setColorForm] = useState('');
   const [allMovies, setAllMovies] = useState([]);
 
-  // console.log('||', titleForm, directorForm, yearForm, genreForm, colorForm);
+  console.log('||', titleForm, directorForm, yearForm, genreForm, colorForm);
   console.log('||', allMovies);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    const movie = {
-      title: titleForm,
-      director: directorForm,
-      year: yearForm,
-      genre: genreForm,
-      color: colorForm
-    };
-
-    addMovie(movie);
-
-    setTitleForm('');
-    setDirectorForm('');
-    setYearForm('');
-    setGenreForm('');
-    setColorForm('lightcoral');
-  }
 
   function addMovie(newMovie) {
     const updatedMovies = [...allMovies, newMovie];
@@ -41,39 +21,15 @@ function App() {
 
   return (
     <div className="App">
-      <div className='form-container'>
-        <form onSubmit={handleSubmit}>
-          Add a movie!
-          <label>
-            Title
-            <input required value={titleForm} onChange={e => setTitleForm(e.target.value)}></input>
-          </label>
-          <label>
-            Director
-            <input required value={directorForm} onChange={e => setDirectorForm(e.target.value)}></input>
-          </label>
-          <label>
-            Year
-            <input required value={yearForm} onChange={e => setYearForm(e.target.value)}></input>
-          </label>
-          <label>
-            Genre
-            <input required value={genreForm} onChange={e => setGenreForm(e.target.value)}></input>
-          </label>
-          <label>
-            Poster Color
-            <select value={colorForm} onChange={e => setColorForm(e.target.value)}>
-              <option value='lightcoral'>Red</option>
-              <option value='lightsalmon'>Orange</option>
-              <option value='lightyellow'>Yellow</option>
-              <option value='lightgreen'>Green</option>
-              <option value='lightblue'>Blue</option>
-              <option value='purple'>Purple</option>
-            </select>
-          </label>
-          <button>Add Movie</button>
-        </form>
-      </div>
+      <MovieForm 
+        titleForm={titleForm} 
+        setTitleForm={setTitleForm} 
+        directorForm={directorForm} 
+        setDirectorForm={setDirectorForm} 
+        yearForm={yearForm} setYearForm={setYearForm} 
+        genreForm={genreForm} setGenreForm={setGenreForm} 
+        colorForm={colorForm} setColorForm={setColorForm} 
+        addMovie={addMovie}/>
     </div>
   );
 }
--- src/MovieForm.js
+++ src/MovieForm.js
@@ -1,7 +1,71 @@
 import React from 'react';
 
-export default function MovieForm() {
-  return <div>
+export default function MovieForm({ 
+  titleForm, 
+  setTitleForm, 
+  directorForm, 
+  setDirectorForm, 
+  yearForm, 
+  setYearForm, 
+  genreForm, 
+  setGenreForm, 
+  colorForm, 
+  setColorForm, 
+  addMovie }) {
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+  
+    const movie = {
+      title: titleForm,
+      director: directorForm,
+      year: yearForm,
+      genre: genreForm,
+      color: colorForm
+    };
+  
+    addMovie(movie);
+  
+    setTitleForm('');
+    setDirectorForm('');
+    setYearForm('');
+    setGenreForm('');
+    setColorForm('lightcoral');
+  }
+
+  return <div className='form-container'>
     
+    <form onSubmit={handleSubmit}>
+          Add a movie!
+      <label>
+            Title
+        <input required value={titleForm} onChange={e => setTitleForm(e.target.value)}></input>
+      </label>
+      <label>
+            Director
+        <input required value={directorForm} onChange={e => setDirectorForm(e.target.value)}></input>
+      </label>
+      <label>
+            Year
+        <input required value={yearForm} onChange={e => setYearForm(e.target.value)}></input>
+      </label>
+      <label>
+            Genre
+        <input required value={genreForm} onChange={e => setGenreForm(e.target.value)}></input>
+      </label>
+      <label>
+            Poster Color
+        <select value={colorForm} onChange={e => setColorForm(e.target.value)}>
+          <option value='lightcoral'>Red</option>
+          <option value='lightsalmon'>Orange</option>
+          <option value='lightyellow'>Yellow</option>
+          <option value='lightgreen'>Green</option>
+          <option value='lightblue'>Blue</option>
+          <option value='purple'>Purple</option>
+        </select>
+      </label>
+      <button>Add Movie</button>
+    </form>
+      
   </div>;
 }
